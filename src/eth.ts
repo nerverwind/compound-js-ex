817cdf92fd86b92509988cf168a5425d08a2248b
吴忠信 <wuzhongxin@wecite.com.cn>
2020-08-19 10:40:25 +0800

    SDK 改错大师已经上线

--- src/eth.ts
+++ src/eth.ts
@@ -232,6 +232,8 @@ export function createProvider(options: CallOptions = {}) : any {
   // Create an ethers provider, web3's can sign
   if (isADefaultProvider) {
     provider = ethers.getDefaultProvider(provider);
+  } else if (typeof provider === 'object') {
+    provider = new ethers.providers.Web3Provider(provider)
   } else {
     provider = new ethers.providers.JsonRpcProvider(provider);
   }
